fb3e9a3bf1e1508221fd8a8226c2bdb28f23e866
Feifei Li <li.feifei.official@gmail.com>
2021-11-11 22:02:26 +0000

    Variational auto-encoder added. Pending.

--- R/GAE.R
+++ R/GAE.R
@@ -159,3 +159,80 @@ GCNModelAE <- torch::nn_module(
     }
 )
 ## TODO: Add Auto-Encoding Variational Bayes as an better alternative to this
+
+#' GCN Model Variational Auto-enccoder auto-encoding variational Bayes
+#'
+#' Auto-encoder for GCN using
+#'
+#' #' @references
+#' \insertRef{GCN}{scRGNet}
+#'
+# @export
+#' @import R6
+#' @import torch
+GCNModelVAE <- torch::nn_module(
+    classname = "GCNModelVAE",
+    public = list(
+        #' @field gc1 the first GCN layer
+        gc1 = NULL,
+        #' @field gc2 the second GCN layer
+        gc2 = NULL,
+        #' @field gc3 the third GCN layer
+        gc3 = NULL,
+        #' @field dc Decoder using inner product for prediction
+        dc  = NULL
+    ),
+    #' @description
+    #' Create an GCN auto-encoder object.
+    #' @param input_feat_dim dimension of input feature matrix
+    #' @param hidden_dim1 dimension of matrix in the first hidden layer
+    #' @param hidden_dim2 dimension of matrix in the second hidden layer
+    #' @param dropout dropout
+    #' @return A new `GCNModelAE` object.
+    initialize = function(input_feat_dim,
+                          hidden_dim1,
+                          hidden_dim2,
+                          dropout) {
+        self$gc1 <- GraphCovolution(input_feat_dim,
+                                    hidden_dim1,
+                                    dropout,
+                                    act = torch::nnf_relu)
+        self$gc2 <- GraphCovolution(hidden_dim1,
+                                    hidden_dim2,
+                                    dropout,
+                                    act = function(x) return(x))
+        self$gc3 <- GraphCovolution(hidden_dim1,
+                                    hidden_dim2,
+                                    dropout,
+                                    act = function(x) return(x))
+        self.dc  <- InnerProductDecoder(dropout, act = function(x) return(x))
+    },
+    encode = function(x, adj) {
+        hidden1 <- self$gc1(x, adj)
+        encoded <- list(
+            self$gc2(hidden1, adj),
+            self@gc3(hidden1, adj)
+        )
+    },
+    reparameterize = function(mu, logvar) {
+        if (self$training) {
+            std <- torch::torch_exp(logvar)
+            eps <- torch::torch_rand_like(std)
+            ## Try addmm if this doesn't work properly
+            return(torch::torch_addcmul(mu, eps, std))
+        }
+    },
+    forward =  function(x, adj) {
+        params <- self$encode(x, adj)
+        mu     <- params[[1]]
+        logvar <- params[[2]]
+        z      <- self$reparameterize(mu, logvar)
+        return(
+            list(
+                z      = z,
+                mu     = mu,
+                logvar = logvar
+            )
+        )
+    }
+)
