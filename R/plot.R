15e50b1be9f226fe1896c1cdc2e44b0bed6035d8
Feifei Li <li.feifei.official@gmail.com>
2021-11-16 02:05:43 +0000

    Making plots look nicer

--- R/plot.R
+++ R/plot.R
@@ -7,6 +7,7 @@
 #'
 #' @export
 #' @import igraph
+#' @importFrom graphics plot
 plotCellNet <- function(net, group = TRUE) {
 
     if (group)
@@ -19,22 +20,44 @@ plotCellNet <- function(net, group = TRUE) {
          )
 }
 
+#' Plot a cell network
+#'
+#' plot
+#'
+#' @param net A graph object
+#'
+#' @export
+#' @importFrom igraph degree
+#' @importFrom graphics hist
 plotDegree <- function(net) {
     d_dist <- igraph::degree(net)
-    hist(d_dist)
+    output <- graphics::hist(d_dist)
+    return(output)
 }
 
+#' Plot a cell network
+#'
+#' plot
+#'
+#' @param net A graph object
+#' @param title Title of the log-log plot
+#'
+#' @export
+#' @importFrom igraph degree
+#' @importFrom graphics plot
 plotLogRank <- function(net, title = "A log-log Plot of Connectivities for Cell Network") {
     dg       <- igraph::degree(net)
     rankFreq <- table(dg) # get rank and frequency of the graph
     freqRank <- as.integer(names(rankFreq)) # seperate ranks from rankFreq
     freq     <- as.integer(rankFreq) # seperate frequency from rankFreq
 
-    plot(log10(freqRank + 1),
-         log10(freq),
-         type = "b", # type b means draw for both lines and dots
-         pch  = 13,   # shape of points
-         bg   = "#A5F5CC",
-         xlab = "log(rank)", ylab = "log(frequency)",
-         main = title)
+    output <- plot(log10(freqRank + 1),
+                   log10(freq),
+                   type = "b", # type b means draw for both lines and dots
+                   pch  = 13,   # shape of points
+                   bg   = "#A5F5CC",
+                   xlab = "log(rank)", ylab = "log(frequency)",
+                   main = title)
+
+    return(output)
 }
