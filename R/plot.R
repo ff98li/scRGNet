eca8df2ba9e712d697e2c89ac916bb627607c338
Feifei Li <li.feifei.official@gmail.com>
2021-12-02 00:55:20 +0000

    Use visNetwork to display an interactive cell network.

--- R/plot.R
+++ R/plot.R
@@ -7,6 +7,8 @@
 #' @param group A logical vector indicating whether to highlight cell communities in the network.
 #'     If FALSE, they will not be highlighted. Default TRUE.
 #' @param title A character vector as the title of the network plot.
+#' @param node_label_size Size of node label
+#' @param node_size Size of node
 #'
 #' @references
 #' \insertRef{igraph}{scRGNet}
@@ -30,11 +32,12 @@
 #' @export
 #' @import igraph
 #' @importFrom Rdpack reprompt
-#' @importFrom graphics plot
 #' @importFrom methods is
 plotCellNet <- function(net,
-                        group = TRUE,
-                        title = "Inferred Cell Network") {
+                        group           = TRUE,
+                        title           = "Inferred Cell Network",
+                        node_label_size = 0.5,
+                        node_size       = 9) {
 
     if (!methods::is(net, "igraph"))
         stop("Invalid argument for net. Must be an igraph object.")
@@ -45,21 +48,47 @@ plotCellNet <- function(net,
     if (!is.character(title))
         stop("Invalid argument for title. Must be a character vector.")
 
+    nodes <- igraph::as_data_frame(net, what = "vertices")
+    edges <- igraph::as_data_frame(net, what = "edges")
+    nodes <- cbind(id = 1:dim(nodes)[1], nodes)
+    colnames(nodes)[1] <- "id"
+    colnames(nodes)[2] <- "label"
+    edges <- merge(x = edges,y = nodes, by.x = "from", by.y = "label", all.x = TRUE)
+    edges <- edges[ , !names(edges) %in% c("from")]
+    edges <- merge(x = edges,y = nodes, by.x = "to", by.y = "label", all.x = TRUE)
+    edges <- edges[ , !names(edges) %in% c("to")]
+    colnames(edges) <- c("from", "to")
+
+    plot_args <- list(
+        node_label_size = node_label_size,
+        node_size       = node_size,
+        title           = title
+    )
+
     if (group) {
-        clp <- igraph::cluster_label_prop(net)
-        plotCellNet_output(clp, net)
-    } else {
-        plotCellNet_output(net)
+        clp                <- igraph::cluster_label_prop(net)
+        group_label        <- data.frame(as.list(igraph::membership(clp)))
+        group_label        <- as.data.frame(t(group_label))
+        group_label$label  <- rownames(group_label)
+        nodes              <- merge(x = nodes, y = group_label, by.x = "label", by.y = "label")
+        colnames(nodes)[3] <- "group"
     }
 
+    visNetwork::visNetwork(nodes = nodes,
+                           edges = edges,
+                           main  = title)
 }
 
-plotCellNet_output <- function(...) {
+## Helper function to handle multiple graph object
+#' @param ...
+#' @NoRd
+#' @import magrittr
+plotCellNet_output <- function(..., plot_args) {
     graphics::plot(...,
-                   vertex.shape     = "none",
-                   vertex.label.cex = 0.5,
-                   vertex.size      = 9,
-                   main             = title
+                   vertex.shape     = plot_args$node_shape,
+                   vertex.label.cex = plot_args$node_label_size,
+                   vertex.size      = plot_args$node_size,
+                   main             = plot_args$title
     )
 }
 
@@ -158,13 +187,13 @@ plotLog <- function(net,
     freqRank <- as.integer(names(rankFreq)) # seperate ranks from rankFreq
     freq     <- as.integer(rankFreq) # seperate frequency from rankFreq
 
-    plot(log10(freqRank + 1),
-         log10(freq),
-         type = "b", # type b means draw for both lines and dots
-         pch  = 13,   # shape of points
-         bg   = "#A5F5CC",
-         xlab = "log(rank)", ylab = "log(frequency)",
-         main = title)
+    graphics::plot(x    = log10(freqRank + 1),
+                   y    = log10(freq),
+                   type = "b", # type b means draw for both lines and dots
+                   pch  = 13,   # shape of points
+                   bg   = "#A5F5CC",
+                   xlab = "log(rank)", ylab = "log(frequency)",
+                   main = title)
 }
 
 # [END]
